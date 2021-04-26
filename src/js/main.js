7c061d78a4944356cae0e03318b2a2cf187c8d77
iTrueStyler <rjcvfnbh100@gmail.com>
2021-04-26 23:04:48 +0300

    add send form data on php server

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import modals from './modules/modals.js';
 import tabs from './modules/tabs.js';
+import forms from './modules/forms.js';
 
 
 window.addEventListener('DOMContentLoaded',()=>{
-    modals(),
-    tabs('.glazing_slider','.glazing_block','.glazing_content','active'),
-    tabs('.decoration_slider','.no_click','.decoration_content>div>div','after_click')
+    modals();
+    tabs('.glazing_slider','.glazing_block','.glazing_content','active');
+    tabs('.decoration_slider','.no_click','.decoration_content>div>div','after_click');
+    forms();
+
 });
