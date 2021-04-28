1c63d9532664fcbc9541b3694d8b04e1d074aceb
iTrueStyler <rjcvfnbh100@gmail.com>
2021-04-28 22:45:19 +0300

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,20 @@ import modals from './modules/modals.js';
 import tabs from './modules/tabs.js';
 import forms from './modules/forms.js';
 import changeModalState from './modules/changeModalState.js';
+import timer from './modules/timer.js';
+
 
 window.addEventListener('DOMContentLoaded',()=>{
 
 
     let modalState={};
+    let deadline='2021-05-08';
+
     changeModalState(modalState);
     modals();
     tabs('.glazing_slider','.glazing_block','.glazing_content','active');
     tabs('.decoration_slider','.no_click','.decoration_content>div>div','after_click');
     tabs('.balcon_icons','.balcon_icons_img','.big_img>img','do_image_more','inline-block');
     forms(modalState);
-
+    timer('.container1',deadline)
 });
